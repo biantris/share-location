3d6e76b5858715c6897ea1721807233b07e0d7c5
beatrizoliveiira <anabeatrizxoliveira@gmail.com>
2022-06-25 15:59:16 -0300

    feat(init): (~˘▾˘)~ share-location

---
+++ README.md
@@ -0,0 +1,7 @@
+### Share Location
+
+- [x] Google Maps API
+- [x] ReactTS
+- [x] ViteJS
+- [ ] Styled Components
+- [ ] Share location with friends
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import MapPage from "./pages/MapPage";
+
+export const REACT_APP_GOOGLE_API_KEY = "";
+
+function App() {
+  return <MapPage />;
+}
+
+export default App;
